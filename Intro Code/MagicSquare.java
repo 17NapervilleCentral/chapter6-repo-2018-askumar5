f4cff9f1c6eea66413431c232772dbb6001e652e
Arjun Kumar <askumar5@users.noreply.github.com>
2018-02-01 08:35:27 -0600

    yugjyu

---
+++ Intro Code/MagicSquare.java
@@ -0,0 +1,30 @@
+//********************************************************************
+//  MagicSquare.java       Author: Arjun Kumar
+//
+//  2/1/2018
+//********************************************************************
+
+import java.text.NumberFormat;
+
+public class MagicSquare
+{
+  public int sumOneRow2DArray(int row)
+    {   int total = 0;
+        
+
+
+
+        return total;
+
+    }
+  public int sumOneCol2DArray(int row)
+    {   int total = 0;
+
+
+
+
+        return total;
+
+    }
+   
+}
