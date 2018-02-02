000098d2fbd72b5dd355972c9c4027864d7a0343
Arjun Kumar <askumar5@users.noreply.github.com>
2018-02-02 08:32:03 -0600

    ekjrger

---
+++ Intro Code/Account.java
@@ -0,0 +1,30 @@
+
+
+/**
+ * Write a description of class Account here.
+ * 
+ * @author (Arjun Kumar) 
+ * @version (2/2/18)
+ */
+public class Account implements Comparable
+{
+    /**
+     * Default constructor for objects of class Account
+     */
+    public Account(){}
+    public Account(String name, int num1, int num2, Address a1)
+    {
+        
+        
+    }
+    public int compareTo(Object other)
+    {
+        
+        
+        
+        return 0;
+    }
+    
+    
+
+}
--- Intro Code/MagicSquare.java
+++ Intro Code/MagicSquare.java
@@ -8,16 +8,49 @@ import java.text.NumberFormat;
 
 public class MagicSquare
 {
-  public int sumOneRow2DArray(int row)
-    {   int total = 0;
+    private MagicSquare mag;
+    private int MagicNum;
+    
+    public MagicSquare()
+    {
         
-
-
+    }
+    private int getMagicNum ( )
+    {
+      int total = 0;
+      //for (int i = 0; i < ; i++)
+      {
+          
+        }
+      
+      
+      
+      return total;
+      
+    }
+    
+    public int addRow(MagicSquare mag, int row)
+    {   int total = 0;
+        for (int i = 0; i <= row.length(); i++)
+        {
+            total += mag[i];
+        }
 
         return total;
 
     }
-  public int sumOneCol2DArray(int row)
+    public int addColumns(MagicSquare mag)
+    {
+        int total = 0;
+        for (int i = 0; i < mag.length; i++)
+        {
+            total += 0;
+        }
+        
+        
+        
+    }
+    public int sumOneCol2DArray(int row)
     {   int total = 0;
 
 
