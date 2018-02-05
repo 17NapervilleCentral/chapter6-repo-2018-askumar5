f6d75af9134ecc3bc22fa0c214e52e74ef3f484a
Arjun Kumar <askumar5@users.noreply.github.com>
2018-02-05 08:35:29 -0600

    mag swuare

--- Intro Code/MagicSquare.java
+++ Intro Code/MagicSquare.java
@@ -8,56 +8,96 @@ import java.text.NumberFormat;
 
 public class MagicSquare
 {
-    private MagicSquare mag;
     private int MagicNum;
-    
-    public MagicSquare()
+    public int[][] magic;
+    public MagicSquare(int[][] array)
     {
-        
+        magic = array;
+       // MagicNum = add up nmbers in the row
     }
+    
+    /**
+     * adds up the first row in the array to get the magic number of the array that the rest of the rows/columns should folllow
+     * @return int, which is the magic num
+     */
     private int getMagicNum ( )
     {
-      int total = 0;
-      //for (int i = 0; i < ; i++)
-      {
-          
-        }
+      int magNum = 0;
       
+      //adds up first row
+      magNum = addRow(0);
       
-      
-      return total;
+      return magNum;
       
     }
     
-    public int addRow(MagicSquare mag, int row)
-    {   int total = 0;
-        for (int i = 0; i <= row.length(); i++)
+    /**
+     * adds up each row/column to check if equal to magic number
+     * @return boolean, if rows and columns = magic number
+     */
+    public boolean isMagic ()
+    {
+        boolean ToF = false;
+        boolean ToF_row = false;
+        boolean ToF_col = false;
+        
+        int magicNum = getMagicNum();
+        int numRows = 0;
+        int numCols = 0;
+        //checks each row
+        for (int r = 0; r <= magic.length; r++)
         {
-            total += mag[i];
+            if (magic.addRow(r) == magicNum)
+                numRows++;
+        }
+        if (numRows == magic.length)
+            ToF_row = true;
+        
+        //checks each column
+        for (int c = 0; c <= magic.length; c++)
+        {
+            if (magic.addColumn(c) == magicNum)
+                numCols++;
+        }
+        if (numCols == magic.length)
+            ToF_col = true;
+        
+        //if both each row and each col all add up to magic num
+        if (ToF_col == true && ToF_row == true)
+            ToF = true;
+        
+        return ToF;
+    }
+    
+    /**
+     * adds up the numbers in each row
+     * @return an int, total of the row
+     */
+    public int addRow(int row)
+    {   int total = 0;
+        for (int c = 0; c <= magic[row].length; c++)
+        {
+            total += magic[row][c];
         }
 
         return total;
 
     }
-    public int addColumns(MagicSquare mag)
+    
+    /**
+     * adds up the numbers in each column
+     * @return int, total of the column
+     */
+    public int addColumn(int col)
     {
         int total = 0;
-        for (int i = 0; i < mag.length; i++)
+        for (int r = 0; r < magic.length; r++)
         {
-            total += 0;
+            total += magic[r][col];
         }
         
-        
-        
-    }
-    public int sumOneCol2DArray(int row)
-    {   int total = 0;
-
-
-
-
         return total;
-
+        
     }
    
 }
